33db5bdee127713b2abd1204914aef31b1d1cce9
Valentin Debon <valentin@debon.net>
2019-08-16 15:55:16 +0200

    Creating sockets youhou

--- Sources/secd/EnsicoinApp.swift
+++ Sources/secd/EnsicoinApp.swift
@@ -6,7 +6,9 @@ public final class EnsicoinApp {
 
 	public final class Config : Codable {
 		public let logger : String = "com.ensicoin.swift"
-		public let port : UInt16 = 4224
+		public let port : Socket.NetPort = 4224
+		public let bufferSize : Int = 4096
+		public let bufferAlignment : Int = 4
 	}
 
 	public static let shared = EnsicoinApp()
@@ -14,6 +16,7 @@ public final class EnsicoinApp {
 	public let config : Config
 	public let logger : Logger
 	public let monitor : FileMonitor
+	public var buffer : UnsafeMutableRawBufferPointer
 
 	private init() {
 		self.config = Config()
@@ -21,7 +24,70 @@ public final class EnsicoinApp {
 		self.logger = Logger(label: self.config.logger)
 
 		self.monitor = FileMonitor()
-		self.monitor.setWrite(of: 1, to: FileWriteBytesDelegate(with: "Hello, World!\n".data(using: .utf8)!))
+
+		self.buffer = UnsafeMutableRawBufferPointer.allocate(byteCount: self.config.bufferSize, alignment: self.config.bufferAlignment)
+
+		let socket = EnsicoinApp.makeSocketIPv6(from: self.config, loggingTo: self.logger)
+		self.monitor.read(for: socket, with: acceptPeerIPv6)
+	}
+
+	deinit {
+		self.buffer.deallocate()
+	}
+
+	private func readPeer(fileDescriptor: FileDescriptor) -> Bool {
+		var address = Socket.NetAddress()
+
+		switch File.receive(socket: fileDescriptor, in: self.buffer, from: &address) {
+		case .failure(let error):
+			logger.error("EnsicoinApp: Unable to receive socket \(fileDescriptor): \(error.rawValue)")
+		case .success(let received):
+			if received == 0 {
+				return true
+			}
+		}
+
+		return false
+	}
+
+	private func acceptPeerIPv6(fileDescriptor: FileDescriptor) -> Bool {
+		var address = Socket.NetAddress()
+
+		switch File.makeSocket(fromINet6: fileDescriptor, address: &address) {
+		case .failure(let error):
+			self.logger.error("EnsicoinApp: Error when accepting socket from INet6 \(fileDescriptor): \(error.rawValue)")
+		case .success(let socket):
+			self.monitor.read(for: socket, with: readPeer)
+		}
+
+		return true
+	}
+
+	private static func makeSocketIPv6(from configuration: Config, loggingTo logger: Logger) -> FileDescriptor {
+
+		guard let netProtocol = Socket.netProtocol(named: "IP"),
+			let netAddress = Socket.NetAddress(ipv6: "::", port: configuration.port) else {
+			logger.error("EnsicoinApp: Unable to load static configuration")
+			abort()
+		}
+
+		switch File.makeSocket(in: .inet6, as: .stream, following: netProtocol) {
+		case .failure(let error):
+			logger.error("EnsicoinApp: Error when creating socket: \(error.rawValue)")
+			abort()
+		case .success(let fileDescriptor):
+			if let error = Socket.bind(socket: fileDescriptor, at: netAddress) {
+				logger.error("EnsicoinApp: Error when binding acceptor: \(error.rawValue)")
+				abort()
+			}
+
+			if let error = Socket.listen(with: fileDescriptor) {
+				logger.error("EnsicoinApp: Error for listen: \(error.rawValue)")
+				abort()
+			}
+
+			return fileDescriptor
+		}
 	}
 }
 
--- Sources/secd/main.swift
+++ Sources/secd/main.swift
@@ -4,12 +4,14 @@ LoggingSystem.bootstrap(StreamLogHandler.standardError)
 
 let app = EnsicoinApp.shared
 
+app.logger.info("EnsicoinApp running...")
+
 while !app.monitor.isEmpty {
 	switch app.monitor.pselect() {
-		case .success:
-			break
-		case .failure(let failure):
-			app.logger.error("Monitor error: \(failure)")
+	case .success(let handled):
+		app.logger.info("EnsicoinApp Monitor handled \(handled) connections")
+	case .failure(let failure):
+		app.logger.error("EnsicoinApp Monitor error: \(failure)")
 	}
 }
 
